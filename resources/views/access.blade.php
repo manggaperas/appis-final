db2e0661ab7fb0fa890af1d3b8d1aabb589e5d4c
manggaperas <muhammadanggaprasetyo12@gmail.com>
2021-12-24 23:46:29 +0700

    update view manager

--- resources/views/access.blade.php
+++ resources/views/access.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="title">
-        Account Manager
+        Account Manager2
     </x-slot>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
