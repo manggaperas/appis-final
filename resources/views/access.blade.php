ee553b5f16a83c60adfe7134e45dc3e1c9bece81
manggaperas <muhammadanggaprasetyo12@gmail.com>
2021-12-27 21:20:49 +0700

    add table account

--- resources/views/access.blade.php
+++ resources/views/access.blade.php
@@ -13,28 +13,64 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     {{-- Pengaturan Lanjut Akun --}}
-                    {{-- <x-backend.card>
-                        <x-slot name="header">
-                            @lang('User Management')
-                        </x-slot>
-                
-                        @if ($logged_in_user->hasAllAccess())
-                            <x-slot name="headerActions">
-                                <x-utils.link
-                                    icon="c-icon cil-plus"
-                                    class="card-header-action"
-                                    :href="route('admin.auth.user.create')"
-                                    :text="__('Create User')"
-                                />
-                            </x-slot>
-                        @endif
-                
-                        <x-slot name="body">
-                            <livewire:users-table />
-                        </x-slot>
-                    </x-backend.card> --}}
+                    <table class="min-w-full border-collapse block md:table">
+                        <thead class="block md:table-header-group">
+                            <tr class="border border-grey-500 md:border-none block md:table-row absolute -top-full md:top-auto -left-full md:left-auto  md:relative ">
+                                <th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Name</th>
+                                <th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">User Name</th>
+                                <th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Email Address</th>
+                                <th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Mobile</th>
+                                <th class="bg-gray-600 p-2 text-white font-bold md:border md:border-grey-500 text-left block md:table-cell">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody class="block md:table-row-group">
+                            <tr class="bg-gray-300 border border-grey-500 md:border-none block md:table-row">
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Name</span>Jamal Rios</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">User Name</span>jrios1</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Email Address</span>jrios@icloud.com</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Mobile</span>582-3X2-6233</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                    <span class="inline-block w-1/3 md:hidden font-bold">Actions</span>
+                                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">Edit</button>
+                                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">Delete</button>
+                                </td>
+                            </tr>
+                            <tr class="bg-white border border-grey-500 md:border-none block md:table-row">
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Name</span>Erwin Campbell</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">User Name</span>ecampbell088</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Email Address</span>ecampbell088@hotmail.com</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Mobile</span>318-685-X414</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                    <span class="inline-block w-1/3 md:hidden font-bold">Actions</span>
+                                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">Edit</button>
+                                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">Delete</button>
+                                </td>
+                            </tr>
+                            <tr class="bg-gray-300 border border-grey-500 md:border-none block md:table-row">
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Name</span>Lillie Clark</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">User Name</span>lillie</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Email Address</span>lillie.clark@gmail.com</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Mobile</span>505-644-84X4</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                    <span class="inline-block w-1/3 md:hidden font-bold">Actions</span>
+                                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">Edit</button>
+                                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">Delete</button>
+                                </td>
+                            </tr>
+                            <tr class="bg-white border border-grey-500 md:border-none block md:table-row">
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Name</span>Maribel Koch</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">User Name</span>maribelkoch</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Email Address</span>mkoch@yahoo.com</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell"><span class="inline-block w-1/3 md:hidden font-bold">Mobile</span>582-400-3X36</td>
+                                <td class="p-2 md:border md:border-grey-500 text-left block md:table-cell">
+                                    <span class="inline-block w-1/3 md:hidden font-bold">Actions</span>
+                                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 border border-blue-500 rounded">Edit</button>
+                                    <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 border border-red-500 rounded">Delete</button>
+                                </td>
+                            </tr>			
+                        </tbody>
+                    </table>
 
-                    <x-table :headings="$headings" :data="$data" />
                 </div>
             </div>
         </div>
