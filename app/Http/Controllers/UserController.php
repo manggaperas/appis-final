44373cb300a2f05eec517209ae11528b39225f93
manggaperas <muhammadanggaprasetyo12@gmail.com>
2021-12-27 20:30:16 +0700

    update user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function index(Request $request) {
+        $data = User::all();
+        $headings = collect([
+            'name' => 'Name',
+            'email' => 'Email'
+        ]);
+        return view('access', compact('data', 'headings'));
+    }
+
+    public function create() {
+        return view('create_access');
+    }
 }
